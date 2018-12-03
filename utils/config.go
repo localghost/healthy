f94de7ce82219d2e7d023f95a040b412862182dc
Zbigniew Kostrzewa <zkostrzewa@gmail.com>
2018-12-03 06:47:25 +0100

    Remove unused function.

--- utils/config.go
+++ utils/config.go
@@ -2,13 +2,6 @@ package utils
 
 import "github.com/mitchellh/mapstructure"
 
-func WithDefault(m map[string]interface{}, k string, d interface{}) interface{} {
-	if v, ok := m[k]; ok {
-		return v
-	}
-	return d
-}
-
 func Decode(input interface{}, output interface{}) error {
 	config := &mapstructure.DecoderConfig{
 		Metadata: nil,
